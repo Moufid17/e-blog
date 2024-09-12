b1f8783c38596428cfd2d94987584bfecc554020
MMoufid17 <mmoutarou1@myges.fr>
2024-09-12 06:26:32 +0200

    update input title focus behavior.

--- app/components/common/postItem.tsx
+++ app/components/common/postItem.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Stack, Typography } from "@mui/joy";
+import { Box, Divider, FormControl, FormLabel, Input, Stack, Typography } from "@mui/joy";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 
@@ -32,18 +32,44 @@ export default function PostItem({ postId = "new" }: { postId?: string }) {
     return (
         <Stack spacing={2} sx={{bgcolor: "#fff"}}>
             { post ? 
-                <Stack key={post.id} alignItems="center" sx={{pt: "1.5rem"}} spacing={3}>
-                    <Stack>
-                        <Typography level="title-lg" alignSelf="center" fontSize="26px">{toUppercaseFirstChar(post?.title ?? "")}</Typography>
-                        <Typography level="body-md" alignSelf="center">Auteur : {toUppercaseFirstChar(post?.owner?.name ?? "") ?? "Esgi"}</Typography>
-                        <Typography level="body-sm" alignSelf="center">Mise à jour : {toUppercaseFirstChar(convertDateToString(post.updatedAt ?? (new Date())))}</Typography>
+                <Stack key={post.id} alignItems="center" sx={{pt: "1.5rem"}} spacing={1}>
+                    <Stack key={"post_title"} width="100%" sx={{p:1}}>
+                        <Stack direction={"row"} spacing={2} width="100%" sx={{pr: 4, justifyContent: "end"}}>
+                            <Typography level="body-md" alignSelf="flex-end">by {toUppercaseFirstChar(post?.owner?.name ?? "") ?? "Esgi"}</Typography>
+                            <Divider orientation="vertical"/>
+                            <Typography level="body-sm" alignSelf="flex-end">{toUppercaseFirstChar(convertDateToString(post.updatedAt ?? (new Date())))}</Typography>
+                        </Stack>
+                        <FormControl required>
+                            <FormLabel>Titre</FormLabel>
+                            <Input disabled={session != null} onChange={(e) => { setPost({...post, title: e.target.value})} } color="neutral" defaultValue={toUppercaseFirstChar(post?.title ?? "")} placeholder="Type your title"
+                                sx={{
+                                    p: 2,
+                                    '&::before': {
+                                        display: 'none',
+                                    },
+                                    '&:focus-within': {
+                                      borderColor: '#0D0D0D',
+                                      outline: '2px solid #0D0D0D',
+                                      outlineOffset: '2px',
+                                    },
+                                }}
+                            />    
+                        </FormControl>
                     </Stack>
-                    {/* Check logged user is owner */}
-                    {(session && session?.user?.email == post?.owner?.email) ? 
-                        <PostEditor data={post} isNew={postId == "new"}/>
-                    : 
-                        <PostViewer content={description} />
-                    }
+                    {/* <Stack direction={"column"} spacing={1} width="100%"> */}
+                    <Stack key={"post_descritption_editor_or_viewer"} width="100%"  spacing={2} sx={{bgcolor: "#fff", p: 1, m: 10}}>
+                        <Box>
+                            <Typography level="body-md"  sx={{mb:"-8px"}}>Description :</Typography>
+                        </Box>
+                        <Box sx={{p:0.5}}>
+                            {/* Check logged user is owner */}
+                            {(session && session?.user?.email == post?.owner?.email) ? 
+                                <PostEditor data={post} isNew={postId == "new"}/>
+                                : 
+                                <PostViewer content={description} />
+                            }
+                        </Box>
+                    </Stack>
                 </Stack>
             : <></>
             }
