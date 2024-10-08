e6263f124da2bd021fa04378e4c07faf73c6388f
MMoufid17 <mmoutarou1@myges.fr>
2024-10-08 21:19:10 +0200

    (new) update ui & ux.

--- app/components/common/postEditor.tsx
+++ app/components/common/postEditor.tsx
@@ -46,7 +46,6 @@ const MenuBar = () => {
               .toggleBold()
               .run()
           }
-          // className={editor.isActive('bold') ? 'is-active' : ''}
         >
           bold
         </Button>
@@ -60,7 +59,6 @@ const MenuBar = () => {
               .toggleItalic()
               .run()
           }
-          // className={editor.isActive('italic') ? 'is-active' : ''}
         >
           italic
         </Button>
@@ -74,7 +72,6 @@ const MenuBar = () => {
               .toggleStrike()
               .run()
           }
-          // className={editor.isActive('strike') ? 'is-active' : ''}
         >
           strike
         </Button>
@@ -88,7 +85,6 @@ const MenuBar = () => {
               .toggleCode()
               .run()
           }
-          // className={editor.isActive('code') ? 'is-active' : ''}
         >
           code
         </Button>
@@ -101,63 +97,37 @@ const MenuBar = () => {
         <Button
           color={editor.isActive('paragraph') ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().setParagraph().run()}
-          // className={editor.isActive('paragraph') ? 'is-active' : ''}
         >
           paragraph
         </Button>
         <Button
           color={editor.isActive('heading', { level: 1 }) ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          // className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
         >
           Title 1
         </Button>
         <Button
           color={editor.isActive('heading', { level: 2 }) ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          // className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
         >
           Title 2
         </Button>
         <Button
           color={editor.isActive('heading', { level: 3 }) ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          // className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
         >
           Title 3
         </Button>
-        {/* <Button
-          color={editor.isActive('heading', { level: 4 }) ? "warning" : "neutral"}
-          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-          // className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-        >
-          h4
-        </Button>
-        <Button
-          color={editor.isActive('heading', { level: 5 }) ? "warning" : "neutral"}
-          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-          // className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
-        >
-          h5
-        </Button>
-        <Button
-          color={editor.isActive('heading', { level: 6 }) ? "warning" : "neutral"}
-          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-          // className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
-        >
-          h6
-        </Button> */}
+        
         <Button
           color={editor.isActive('bulletList') ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().toggleBulletList().run()}
-          // className={editor.isActive('bulletList') ? 'is-active' : ''}
         >
           bullet list
         </Button>
         <Button
           color={editor.isActive('orderedList') ? "warning" : "neutral"}
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          // className={editor.isActive('orderedList') ? 'is-active' : ''}
         >
           ordered list
         </Button>
@@ -207,16 +177,8 @@ const MenuBar = () => {
         >
           redo
         </Button>
-        {/* <Button
-          color={editor.isActive('bold') ? "warning" : "neutral"}
-          onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-          className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
-        >
-          purple
-        </Button> */}
       </Stack>
       <hr/>
-
     </Stack>
   )
 }
