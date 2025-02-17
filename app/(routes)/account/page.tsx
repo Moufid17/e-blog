b2dda3d924e0ec72b694b78f87990e0499ea979e
MMoufid17 <mmoutarou1@myges.fr>
2025-02-17 11:21:43 +0100

    Account : set "account_card_profil". Responsive and mobile first.

--- app/(routes)/account/page.tsx
+++ app/(routes)/account/page.tsx
@@ -1,9 +1,12 @@
-import { Avatar, Box, List, ListItem, ListItemContent, ListItemDecorator, Stack, Typography } from "@mui/joy";
-import { getServerSession } from "next-auth";
+// This page will split into PROFILE (https://berrydashboard.io/apps/blog/general-settings) and dashboard (https://berrydashboard.io/dashboard/blog)
+import { Avatar, Box, Card, CardContent, Chip, Grid, List, ListItem, ListItemContent, ListItemDecorator, Stack, Typography } from "@mui/joy";
+import { getServerSession, Session } from "next-auth";
 import { redirect } from "next/navigation";
 import type { Metadata } from 'next'
 
 import authOptions from "@/app/lib/authOptions";
+import Link from "next/link";
+
 
 
 export const metadata: Metadata = {
@@ -12,7 +15,7 @@ export const metadata: Metadata = {
 }
 
 export default async function AccountPage () {
-    const session = await getServerSession(authOptions)
+    const session : Session | null = await getServerSession(authOptions)
     
     // Redirect if user session is not defined
     if (!session) {
@@ -20,22 +23,40 @@ export default async function AccountPage () {
     }
     
     return (
-        <Box key="account" component={'main'} sx={{p: 4, gap: 2}}>
-            <Typography id="esgiblog-account-label" level="body-lg" textTransform="uppercase" sx={{ letterSpacing: '0.15rem', }}>
-                Profile
-            </Typography>
-            <List>
-                <ListItem>
-                    <ListItemDecorator>
-                        <Avatar src={session.user?.image ?? "P"}/>
-                    </ListItemDecorator>
-                    <ListItemContent>
-                        <Typography level="body-lg" fontWeight='bold' textTransform="uppercase" >{session.user?.name ?? "John Deo"}</Typography>
-                        <Typography level="body-md" noWrap>{session.user?.email ?? "johndeo@gmail.com"}</Typography>
-                    </ListItemContent>
-                </ListItem>
-            </List>
-        </Box>
+        <Grid key="account_main" component={'main'} container spacing={2} sx={{ flexGrow: 1, p: 2, bgcolor: "background.body",}}>
+            <Grid key="account_card_profil" xs={12} md={3}>
+                <Card key="account_card" sx={{height: "100%"}}>
+                    <Stack key="profil_stack" direction={{xs: "column", xl:"row"}} sx={{gap: 2, alignItems: 'center',}}>
+                        <Box key="profil_stack_box" sx={{justifyContent: 'space-around',}}>
+                            <List key="profil_list" sx={{alignItems: 'center', flexDirection: {xs: "column", lg: "row"}}}>
+                                <ListItem>
+                                    <ListItemContent>
+                                        <Avatar src={session.user?.image ?? "P"}/>
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem sx={{gap: 2, alignItems: 'center',  justifyContent: "center", direction: {md: "column", lg: "row"}}}>
+                                    <ListItemContent sx={{textAlign: {xs: "center", lg: "left"}}}>
+                                        <Typography level="body-lg" fontWeight='bold' textTransform="uppercase" >{session.user?.name ?? "John Deo"}</Typography>
+                                        <Typography level="body-md" noWrap>{session.user?.email ?? "johndeo@gmail.com"}</Typography>
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </Box>
+                        <Box key="profil_tag">
+                            <Chip key="profil_tag_chip" color="primary" variant="solid">Pro</Chip>
+                        </Box>
+                    </Stack>
+                    <CardContent>
+                        
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid xs={12} md={9}>
+                <Card title="D" sx={{height: "100%"}}>
+                    
+                </Card>
+            </Grid>
+        </Grid>
     );
 }
 
