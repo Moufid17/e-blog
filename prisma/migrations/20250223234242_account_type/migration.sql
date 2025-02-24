9ac309e290d1002ce6463d24b282854317a19314
Moufid17 <moufid.mtr@gmail.com>
2025-02-24 01:14:45 +0100

    schema : update. Category seed.

--- prisma/migrations/20250223234242_account_type/migration.sql
+++ prisma/migrations/20250223234242_account_type/migration.sql
@@ -33,6 +33,20 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "role" TEXT NOT NULL DEFAULT '75',
+    "jobName" TEXT,
+    "socialBio" TEXT,
+    "socialLinkedin" TEXT,
+    "socialGithub" TEXT,
+    "socialTwitter" TEXT,
+    "socialWebsite" TEXT,
+    "socialInstagram" TEXT,
+    "socialFacebook" TEXT,
+    "socialYoutube" TEXT,
+    "socialTwitch" TEXT,
+    "socialDiscord" TEXT,
+    "location" TEXT,
+    "type" TEXT NOT NULL DEFAULT 'basic',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -50,8 +64,29 @@ CREATE TABLE "Post" (
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "isPublished" BOOLEAN NOT NULL DEFAULT false,
+    "categoryId" TEXT NOT NULL,
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PostLikes" (
+    "postId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "PostLikes_pkey" PRIMARY KEY ("postId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -70,7 +105,10 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Post_userId_key" ON "Post"("userId");
+CREATE UNIQUE INDEX "Post_id_key" ON "Post"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -80,3 +118,12 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostLikes" ADD CONSTRAINT "PostLikes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostLikes" ADD CONSTRAINT "PostLikes_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,36 @@
-// import { prismaClientDB } from '@/app/lib/prismaClient'
 import { PrismaClient } from '@prisma/client'
 const prismaClientDB = new PrismaClient()
 async function main() {
-  
-    const User1 = await prismaClientDB.user.upsert({
-        where: { email: 'moufid.mtr@gmail.com' },
-        update: {},
-        create: {
-          email: 'moufid.mtr@gmail.com',
-          name: 'Moufid Mtr',
-          posts: {
-            create: {
-              title: 'Check out Prisma with Next.js',
-              description: "The easiest way to work with a database in Next.js/nQuery data from MySQL, PostgreSQL & SQL Server databases in Next.js apps with Prisma — a better ORM for JavaScript and TypeScript.",
-            },
-          },
-        },
-      })
+  const CATEGORIES = [
+    {
+      name: 'UX/UI',
+      color: "primary.solidDisabledBg",
+    },
+    {
+      name: 'Frontend Developer',
+      color: "success.solidDisabledBg",
+    },
+    {
+      name: 'Backend Developer',
+      color: "neutral.solidDisabledBg",
+    },
+    {
+      name: 'DevOps',
+      color: "warning.solidDisabledBg",
+    },
+    {
+      name: 'Data Science',
+      color: "info.solidDisabledBg",
+    }
+  ]
 
-    const User2 = await prismaClientDB.user.upsert({
-        where: { email: 'moufid.moutarou.pro@gmail.com' },
-        update: {},
-        create: {
-          email: 'moufid.moutarou.pro@gmail.com',
-          name: 'Moufid Pro',
-          posts: {
-            create: [
-                {
-                    title: "Comment faire de bons formulaires en React ?",
-                    description: "On dit qu'un développeur ne fait que des formulaires. Donc il vaut mieux savoir faire de bons formulaires et pour ça il faut comprendre les formulaires en React.",
-                },
-                {
-                    title: 'React Query vs SWR',
-                    description: "React Query et SWR sont deux librairies qui permettent de gérer les requêtes HTTP dans une application React. Dans cet article, nous allons les comparer et voir leurs avantages et inconvénients.",
-                }
-            ]
-          },
-        },
-      })
-  
-  
-  console.log({ User1, User2 })
+  CATEGORIES.map(async (category) => {
+    await prismaClientDB.category.upsert({
+      where: { name: category.name },
+      update: {},
+      create: category
+    })
+  })
 }
 
 main()
