e1b90274e2e3f9024f96f4983607b90de5cd9077
Moufid17 <moufid.mtr@gmail.com>
2025-02-27 11:59:54 +0100

    fix Account : re-render des données du <AccountProfileCard/> lorsque s'est props changent. Par exemple le like. Si je like un de mes articles et je viens sur ma page Account, la valeur du like ne s'incrémentaitait pas. Solution : utiliser un useState pour surveiller prendre en compte le changement de userDetails dans un useEffect avant de le re-render(refresh). Je n'utilise pas directement useDetails dans l'effet de bord parce qu'il provoque une boucle infinie : ie chaque fois que userDetails change, cela provoque un nouveau render (à cause de router.refresh()) et une nouvelle mise à jour de userDetails, donc un nouveau render et ainsi de suite. Donc, avec userDetailsState, si userDetails change, on vraiment en terme de ref et pas à cause un rendu de page, on re-render le composant.

--- app/components/account/AccountProfileCard.tsx
+++ app/components/account/AccountProfileCard.tsx
@@ -1,4 +1,8 @@
+"use client"
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import { Box, Card, List, Stack, ListItem, ListItemContent, Avatar, Typography, Chip, Divider, CardContent, CardOverflow, CardActions } from "@mui/joy";
 import { Mail, Linkedin, MapPin, GitHub, Youtube } from "react-feather";
 
@@ -7,6 +11,11 @@ import { toUppercaseFirstChar } from "@/app/lib/utils";
 import { DEFAULT_ACCOUNT_PRIVILEGE, DEFAULT_AVATAR_IMAGE, DEFAULT_EMAIL, DEFAULT_GITHUB, DEFAULT_JOB_NAME, DEFAULT_LINKEDIN, DEFAULT_LOCATION, DEFAULT_PSEUDO, DEFAULT_YOUTUBE } from "@/app/help/constants";
 
 export default function AccountProfileCard ({userDetails} : {userDetails: AccoutProfilType}) {
+    const router = useRouter()
+    const [userDetailsState, _] = useState(userDetails)
+    useEffect(() => {
+        router.refresh()
+    }, [userDetailsState])
     return(
         <Card key="account_profil_card" sx={{ height: "auto" }}>
             <Stack key="account_profil_card_stack" direction={{ xs: "column", xl: "row" }} sx={{ gap: 2, alignItems: 'center', justifyContent: "space-between" }}>
@@ -14,19 +23,19 @@ export default function AccountProfileCard ({userDetails} : {userDetails: Accout
                     <List key="profil_list" sx={{ alignItems: 'center', flexDirection: { xs: "column", lg: "row" } }}>
                         <ListItem key="profil_list_item_0" >
                             <ListItemContent>
-                                <Avatar src={userDetails?.image ?? DEFAULT_AVATAR_IMAGE} />
+                                <Avatar src={userDetailsState?.image ?? DEFAULT_AVATAR_IMAGE} />
                             </ListItemContent>
                         </ListItem>
                         <ListItem key="profil_list_item_1"  sx={{ gap: 2, alignItems: 'center', justifyContent: "center", direction: { md: "column", lg: "row" } }}>
                             <ListItemContent sx={{ textAlign: { xs: "center", lg: "left" } }}>
-                                <Typography level="body-lg" fontWeight='bold' textTransform="uppercase">{userDetails?.name ?? DEFAULT_PSEUDO}</Typography>
-                                <Typography level="body-md" noWrap>{userDetails?.jobName ?? DEFAULT_JOB_NAME}</Typography>
+                                <Typography level="body-lg" fontWeight='bold' textTransform="uppercase">{userDetailsState?.name ?? DEFAULT_PSEUDO}</Typography>
+                                <Typography level="body-md" noWrap>{userDetailsState?.jobName ?? DEFAULT_JOB_NAME}</Typography>
                             </ListItemContent>
                         </ListItem>
                     </List>
                 </Box>
                 <Box key="profil_tag">
-                    <Chip key="profil_tag_chip" color="primary" variant="solid" sx={{ fontSize: { xs: "lg", lg: "md" } }}>{toUppercaseFirstChar(userDetails.accountPrivilege as string) ?? DEFAULT_ACCOUNT_PRIVILEGE}</Chip>
+                    <Chip key="profil_tag_chip" color="primary" variant="solid" sx={{ fontSize: { xs: "lg", lg: "md" } }}>{toUppercaseFirstChar(userDetailsState.accountPrivilege as string) ?? DEFAULT_ACCOUNT_PRIVILEGE}</Chip>
                 </Box>
             </Stack>
             <Divider inset="context" />
@@ -35,27 +44,27 @@ export default function AccountProfileCard ({userDetails} : {userDetails: Accout
                     <List key="profil_card_content_list" sx={{ display: 'flex', alignItems: "stretch", justifyContent: 'center', gap: 1 }}>
                         <ListItem key="profil_card_content_list_0" sx={{ justifyContent: "space-between", alignItems: 'center' }}>
                             <Typography level="body-md" noWrap textAlign={"left"} startDecorator={<Mail />}>Email</Typography>
-                            <Typography level="body-md" noWrap>{userDetails?.email ?? DEFAULT_EMAIL}</Typography>
+                            <Typography level="body-md" noWrap>{userDetailsState?.email ?? DEFAULT_EMAIL}</Typography>
                         </ListItem>
                         <Divider />
                         <ListItem key="profil_card_content_list_1" sx={{ justifyContent: "space-between", alignItems: 'center' }}>
                             <Typography level="body-md" noWrap textAlign={"left"} startDecorator={<Linkedin />}>LinkedIn</Typography>
-                            <Link href={userDetails.socialLink?.linkedin ?? DEFAULT_LINKEDIN}target="_blank">{"linkedin/me"}</Link>
+                            <Link href={userDetailsState.socialLink?.linkedin ?? DEFAULT_LINKEDIN}target="_blank">{"linkedin/me"}</Link>
                         </ListItem>
                         <Divider />
                         <ListItem key="profil_card_content_list_2" sx={{ justifyContent: "space-between", alignItems: 'center' }}>
                             <Typography level="body-md" noWrap textAlign={"left"} startDecorator={<Youtube />}>Youtube</Typography>
-                            <Link href={userDetails.socialLink?.youtube ?? DEFAULT_YOUTUBE} target="_blank"><Typography level="body-md">{"youtube/me"}</Typography></Link>
+                            <Link href={userDetailsState.socialLink?.youtube ?? DEFAULT_YOUTUBE} target="_blank"><Typography level="body-md">{"youtube/me"}</Typography></Link>
                         </ListItem>
                         <Divider />
                         <ListItem key="profil_card_content_list_3" sx={{ justifyContent: "space-between", alignItems: 'center' }}>
                             <Typography level="body-md" noWrap textAlign={"left"} startDecorator={<GitHub />}>GitHub</Typography>
-                            <Link href={userDetails.socialLink?.github ?? DEFAULT_GITHUB} target="_blank">{"github/me"}</Link>
+                            <Link href={userDetailsState.socialLink?.github ?? DEFAULT_GITHUB} target="_blank">{"github/me"}</Link>
                         </ListItem>
                         <Divider />
                         <ListItem key="profil_card_content_list_4" sx={{ justifyContent: "space-between", alignItems: 'center' }}>
                             <Typography level="body-md" noWrap textAlign={"left"} startDecorator={<MapPin />}>Location</Typography>
-                            <Typography level="body-md" noWrap textAlign={"left"}>{userDetails.location ?? DEFAULT_LOCATION}</Typography>
+                            <Typography level="body-md" noWrap textAlign={"left"}>{userDetailsState.location ?? DEFAULT_LOCATION}</Typography>
                         </ListItem>
                     </List>
                 </Box>
@@ -63,7 +72,7 @@ export default function AccountProfileCard ({userDetails} : {userDetails: Accout
             <CardOverflow>
                 <CardActions orientation="horizontal" sx={{ display: 'flex', justifyContent: 'space-around' }}>
                     <Box key={""} component={'span'} sx={{ display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: 'center' }}>
-                        <Typography level="title-lg">{userDetails?.stats?.likes ?? "-"}</Typography>
+                        <Typography level="title-lg">{userDetailsState?.stats?.likes ?? "-"}</Typography>
                         <Typography level="body-xs">like(s)</Typography>
                     </Box>
                     {["comment(s)", "view(s)"].map((t, index) => (
