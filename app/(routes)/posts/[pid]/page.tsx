133929e7eeed6cf449a798ac34e1ae297ca8fea6
Moufid17 <moufid.mtr@gmail.com>
2025-04-26 00:51:43 +0200

    Remove routing post by id.

--- app/(routes)/posts/[pid]/page.tsx
+++
@@ -1,7 +0,0 @@
-import PostItemExperimental from "@/app/components/common/PostItemExperimental";
-
-export default function Post({ params }: { params: { pid: string } }) {
-    return(
-        <PostItemExperimental postId={params.pid} />
-    )
-}
